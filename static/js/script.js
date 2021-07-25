866926043e375f5efdc6a225a32f8ea510f846a5
dsteyn44 <d_steyn@yahoo.com>
2021-07-25 19:19:16 +0000

    Add styling to add_comics.html

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+    $('.datepicker').datepicker({
+      format: "dd mmmm, yyyy",
+      yearRange: 100,
+      showClearBtn: true,
+      i18n: {
+          done: "Select"
+      }
+    });
   });
